db949f04c8d8ec62a85b950893b8253814846eed
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-06 14:30:38 +0200

    add update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
 
   get 'task/new', to: 'tasks#new', as: :new
-
   post 'tasks', to: 'tasks#create'
+
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
 end
